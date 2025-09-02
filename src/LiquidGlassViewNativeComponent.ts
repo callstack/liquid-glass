fc00f41d21d5ca727a6701e5e2ed76bb80e74fac
Satyajit Sahoo <satyajit.happy@gmail.com>
2025-09-02 20:27:01 +0200

    docs: add JSDocs for better DX (#2)

--- src/LiquidGlassViewNativeComponent.ts
+++ src/LiquidGlassViewNativeComponent.ts
@@ -6,9 +6,31 @@ import {
 } from 'react-native';
 
 export interface NativeProps extends ViewProps {
+  /**
+   * Make the view respond to user interactions.
+   * Interactive view grow on touch and show a shimmer effect.
+   *
+   * Defaults to `false`.
+   */
   interactive?: boolean;
+  /**
+   * The variant of the liquid glass material.
+   *
+   * Defaults to 'regular'.
+   */
   effect?: CodegenTypes.WithDefault<'clear' | 'regular', 'regular'>;
+  /**
+   * The color of the glass effect.
+   *
+   * Defaults to `transparent`.
+   */
   tintColor?: ColorValue;
+  /**
+   * The color scheme of the glass effect.
+   * The effect appears dark or light based on the color scheme.
+   *
+   * Defaults to 'system'.
+   */
   colorScheme?: CodegenTypes.WithDefault<'light' | 'dark' | 'system', 'system'>;
 }
 
