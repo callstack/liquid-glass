13158eccad53632e851d01f87f7a5a1d392f4600
Oskar Kwaśniewski <oskarkwasniewski@icloud.com>
2025-10-02 17:40:38 +0200

    fix: move off update mask for setting up view

--- ios/LiquidGlassView.swift
+++ ios/LiquidGlassView.swift
@@ -26,9 +26,15 @@ import UIKit
 @available(iOS 26.0, *)
 @objc public class LiquidGlassViewImpl: UIVisualEffectView {
   private var isFirstMount: Bool = true
-  @objc public var effectTintColor: UIColor?
-  @objc public var interactive: Bool = false
-  @objc public var style: LiquidGlassEffect = .regular
+  @objc public var effectTintColor: UIColor? {
+    didSet { self.setupView() }
+  }
+  @objc public var interactive: Bool = false {
+    didSet { self.setupView() }
+  }
+  @objc public var style: LiquidGlassEffect = .regular {
+    didSet { self.setupView() }
+  }
 
   public override func layoutSubviews() {
     if (self.effect != nil) { return }
