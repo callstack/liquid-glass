de23e1695bea39b5ac24727811376f3e5dc08565
Gabriel Meola <gabemeola@gmail.com>
2025-12-04 05:47:42 -0600

    fix: macos catalyst compatibility (#35)
    Defaults `UIDesignRequiresCompatibility` to `false` if not found

--- ios/LiquidGlassModule.mm
+++ ios/LiquidGlassModule.mm
@@ -9,7 +9,7 @@
 #if defined(__IPHONE_OS_VERSION_MAX_ALLOWED) && __IPHONE_OS_VERSION_MAX_ALLOWED >= 260000 /* __IPHONE_26_0 */
   if (@available(iOS 26.0, *)) {
     NSDictionary *infoPlist = [[NSBundle mainBundle] infoDictionary];
-    BOOL requiresDesignCompatibility = infoPlist[@"UIDesignRequiresCompatibility"];
+    BOOL requiresDesignCompatibility = [infoPlist[@"UIDesignRequiresCompatibility"] boolValue];
     
     _constants = facebook::react::typedConstants<JS::NativeLiquidGlassModule::Constants>({
       .isLiquidGlassSupported = !requiresDesignCompatibility
